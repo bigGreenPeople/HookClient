23467f4ae2b4998441e3ed015c6cd2218caa4de7
fujie123 <1243596620@qq.com>
2020-04-27 17:27:12 +0800

    init

---
+++ HookClient/HookClientDlg.h
@@ -0,0 +1,33 @@
+﻿
+// HookClientDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CHookClientDlg 对话框
+class CHookClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CHookClientDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HOOKCLIENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
